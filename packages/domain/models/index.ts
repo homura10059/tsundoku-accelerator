2c5659ac727ec47f71603fd96728b64b188cae51
o-hayato <preasper0+github@gmail.com>
2022-01-22 20:27:25 +0900

    add domain

---
+++ packages/domain/models/index.ts
@@ -0,0 +1,28 @@
+import { definitions } from './generated/supabase'
+
+export type Item = definitions['items']
+
+export type ItemHistory = definitions['itemHistories']
+
+export type ItemDetail = definitions['items'] & {
+  itemHistories: ItemHistory[]
+}
+
+export type WishList = definitions['wishLists']
+
+export type WishListDetail = WishList & {
+  wishLists_to_items: {
+    wishListId: string
+    itemId: string
+    items: Item
+  }[]
+}
+
+export type User = {
+  id: string
+  users_to_wishLists: {
+    userId: string
+    wishListId: string
+    wishLists: WishListDetail[]
+  }[]
+}
