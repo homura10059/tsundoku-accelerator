b1f4d6d88efffac22949cbf19afaf5b68fb48dd8
o-hayato <preasper0+github@gmail.com>
2022-02-06 13:43:02 +0900

    add storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,10 +1,18 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
+  stories: [
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-};
+  addons: [
+      '@storybook/addon-links',
+      '@storybook/addon-essentials',
+      {
+        name: '@storybook/addon-postcss',
+        options: {
+          postcssLoaderOptions: {
+            implementation: require('postcss'),
+          },
+        },
+      },
+  ]
+}
